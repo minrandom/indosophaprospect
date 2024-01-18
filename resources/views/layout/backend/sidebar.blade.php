e020f764f6a4bc0e1f19d42d8531212b56936d29
Jojo Pattinama <pattinamajojo@gmail.com>
2024-01-18 15:07:31 +0700

    jadwal route fix

--- resources/views/layout/backend/sidebar.blade.php
+++ resources/views/layout/backend/sidebar.blade.php
@@ -42,7 +42,7 @@
         <div id="collapseJadwal" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Jadwal Menu:</h6>
-                <a class="collapse-item" href="../admin/jadwal">
+                <a class="collapse-item" href="{{ route('schedule') }}">
             <i class="fas fa-fw fa-calendar"></i>
 <span>Penjadwalan</span></a>
             </div>
