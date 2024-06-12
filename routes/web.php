d0786e58add515418afe2f572b7288640a358fd0
Jojo Pattinama <pattinamajojo@gmail.com>
2024-06-12 09:58:27 +0700

    dba prod cat

--- routes/web.php
+++ routes/web.php
@@ -127,8 +127,8 @@ Route::group(['namespace' => 'Admin','middleware' => 'auth','prefix' => 'admin']
 	Route::get('/hospital/{provinceId}/hospital',[HospitalController::class,'getHospitalsByProvince'])->name('admin.getHospitalsByProvince')->middleware(['auth', 'role:admin,fs,am,nsm,bu']);
 	Route::get('/hospital2/{provinceId}/hospital2',[HospitalController::class,'getHospitalsByProvince2'])->name('admin.getHospitalsByProvince2')->middleware(['auth', 'role:admin,fs,am,nsm,bu']);
 	Route::get('/dept/{hospitalId}/dept',[DeptValidController::class,'getDeptValid'])->name('admin.getDeptValid')->middleware(['auth', 'role:admin,fs,am,nsm,bu']);
-	Route::POST('/deptvalid',[DeptValidController::class,'store'])->name('admin.deptvalid')->middleware(['auth', 'role:admin,fs,am,nsm,bu']);
-	Route::get('/category/{unitId}/category',[CategoryController::class,'getCategoriesByUnit'])->name('admin.getCategoriesByUnit')->middleware(['auth', 'role:admin,fs,am,nsm,bu']);
+	Route::POST('/deptvalid',[DeptValidController::class,'store'])->name('admin.deptvalid')->middleware(['auth', 'role:admin,fs,am,nsm,bu,dba']);
+	Route::get('/category/{unitId}/category',[CategoryController::class,'getCategoriesByUnit'])->name('admin.getCategoriesByUnit')->middleware(['auth', 'role:admin,fs,am,nsm,bu,dba']);
 	Route::get('/categorydata/{unitId}/category',[CategoryController::class,'getCatname'])->name('admin.getCatname')->middleware(['auth', 'role:admin,fs,am,nsm,bu']);
 	Route::get('/config/get-products', [ConfigController::class, 'getProducts'])->name('product.getProducts');
 	Route::resource('/user','UserController')->middleware(['can:admin']);
