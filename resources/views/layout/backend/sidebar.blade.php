7abc8714f32125af3361168953106c9f0f9230bf
Jojo Pattinama <pattinamajojo@gmail.com>
2024-12-10 15:39:55 +0700

    user menu

--- resources/views/layout/backend/sidebar.blade.php
+++ resources/views/layout/backend/sidebar.blade.php
@@ -35,11 +35,22 @@
 
     @can('admin')
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSetup"
+       <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSetup"
             aria-expanded="true" aria-controls="collapseSetup">
-            <i class="fas fa-fw fa-toolbox"></i>
+            <i class="fas fa-f w fa-toolbox"></i>
             <span>SETUP DATA</span>
         </a>
+        <div id="collapseSetup" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Setup User Menu</h6>
+                <a class="collapse-item" href="{{ route('admin.user.index') }}">
+            <i class="fas fa-fw fa-user"></i>
+            <span>User List</span></a>
+             
+     
+            </div>
+        </div>
+
         <div id="collapseSetup" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Setup Hospital Menu</h6>
