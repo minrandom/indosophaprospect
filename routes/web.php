fcd1134509877550e4c250177f6a79f141c75281
minrandom <pattinamajojo@gmail.com>
2026-08-10 15:49:09 +0700

    fix double name user

--- routes/web.php
+++ routes/web.php
@@ -320,11 +320,11 @@ Route::get('/scheduledata','ScheduleController@index')->name('schedule.index');
 Route::PATCH('/scheduleupdate/{schedule}','ScheduleController@update')->name('events.update');
 Route::POST('/schedulestore','ScheduleController@store')->name('events.store');
 
-Route::group(['namespace' => 'User','middleware' => 'auth' ,'prefix' => 'fs'],function(){
-	Route::get('/',[UserController::class,'index'])->name('userr');
-	//Route::get('/profile',[ProfileController::class,'index'])->name('profile');
-	//Route::patch('/profile/update/{user}',[ProfileController::class,'update'])->name('profile.update');
-});
+// Route::group(['namespace' => 'User','middleware' => 'auth' ,'prefix' => 'fs'],function(){
+// 	Route::get('/',[UserController::class,'index'])->name('userr');
+// 	//Route::get('/profile',[ProfileController::class,'index'])->name('profile');
+// 	//Route::patch('/profile/update/{user}',[ProfileController::class,'update'])->name('profile.update');
+// });
 
 Route::group(['namespace' => 'User','middleware' => 'auth' ,'prefix' => 'user'],function(){
 	Route::get('/',[UserController::class,'index2'])->name('user');
